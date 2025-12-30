c66ef48188b4b96a9f25fae9e03c987e7db4843c
Sunil Solanki <sunilso9644@gmail.com>
2025-12-30 12:30:26 +0300

    chore: remove unused `structuredClone` global from ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -23,13 +23,12 @@ export default [
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
-      globals: {
-        window: "readonly",
-        document: "readonly",
-        console: "readonly",
-        global: "readonly",
-        structuredClone: "readonly"
-      }
+    globals: {
+      window: "readonly",
+      document: "readonly",
+      console: "readonly",
+      global: "readonly"
+    }
     },
     rules: {
       "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
